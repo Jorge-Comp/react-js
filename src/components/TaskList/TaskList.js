1732753417151edc94fee80d50d7f1957df7ff30
Jorge-Comp <69852368+Jorge-Comp@users.noreply.github.com>
2021-01-08 17:52:43 -0300

    Initial commit

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./tasklist.css";
+import PropTypes from "prop-types";
+import TaskItem from "../TaskItem/TaskItem";
+import plusIcon from "../../img/12025185201606781781.svg";
+export default function TaskList({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const addTask = () => {
+    onAddTask("Nova Tarefa", taskState);
+  };
+
+  return (
+    <div className="tasklist">
+      <div className="title">{title}</div>
+      <div className="content">
+        {tasks.map((task) => {
+          return (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              taskState={taskState}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
+            />
+          );
+        })}
+        {tasks.length === 0 && <div className="empty-list">Lista Vazia</div>}
+        <button onClick={addTask} className="btn">
+          <img src={plusIcon} alt="plus" />
+          dicionar Tarefa
+        </button>
+      </div>
+    </div>
+  );
+}
+
+TaskList.PropTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired
+};
